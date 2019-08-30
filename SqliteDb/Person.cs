51ac0cfa1525c5a580dd986032b6234350962df5
Rathod <abr917209@tatatechnologies.com>
2019-08-30 16:04:07 +0530

    Add project files.

---
+++ SqliteDb/Person.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+namespace SqliteDb
+{
+    public class Person
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+    }
+}
